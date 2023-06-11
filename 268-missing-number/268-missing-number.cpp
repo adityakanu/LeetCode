ecc8979f6c2a5367aeb516f20a8d1dbb423e2053
Aditya Kanu <81138360+adityakanu@users.noreply.github.com>
2023-06-11 23:35:22 +0530

    Time: 27 ms (27.43%), Space: 17.9 MB (50.03%) - LeetHub

---
+++ 268-missing-number/268-missing-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i<n; i++){
+            if (nums[i]!=i){
+                return i;
+                break;
+            }
+            else if(i==n-1){
+                return ++i;
+            }
+        }
+        return 1;
+    }
+};
