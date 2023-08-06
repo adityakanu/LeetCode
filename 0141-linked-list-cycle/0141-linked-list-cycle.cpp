44718798cb3786ded92339fa01e92d325f902e15
Aditya Kanu <81138360+adityakanu@users.noreply.github.com>
2023-08-06 13:52:58 +0530

    Time: 5 ms (96.60%), Space: 8.1 MB (32.96%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,17 +9,29 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*, int > mp;
+        // if head is NULL then return false;
+        if(head == NULL)
+            return false;
         
-        while(head != nullptr){
-            if(mp.find(head)!=mp.end()){
+		// making two pointers fast and slow and assignning them to head
+        ListNode *fast = head;
+        ListNode *slow = head;
+        
+		// till fast and fast-> next not reaches NULL
+		// we will increment fast by 2 step and slow by 1 step
+        while(fast != NULL && fast ->next != NULL)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+			
+			// At the point if fast and slow are at same address
+			// this means linked list has a cycle in it.
+            if(fast == slow)
                 return true;
-            }
-            else{
-                mp[head]++;
-                head = head->next;
-            }
         }
+        
+		// if traversal reaches to NULL this means no cycle.
         return false;
     }
 };
