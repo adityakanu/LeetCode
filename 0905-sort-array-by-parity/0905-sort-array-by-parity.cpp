454a77672355d5cc8c3b1fb9bcdec28330c443de
Aditya Kanu <81138360+adityakanu@users.noreply.github.com>
2023-09-28 12:47:41 +0530

    Time: 4 ms (81.25%), Space: 16.5 MB (36.74%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -5,10 +5,7 @@ public:
         int j = 0;
         
         for(int i = 0; i<n; i++){
-            if(nums[i] & 1){
-                
-            }
-            else{
+            if(nums[i] %2 == 0){
                 swap(nums[i], nums[j]);
                 j++;
             }
