9d7d7a4a95bff6e37b5f039918403682699e7cc9
Aditya Kanu <81138360+adityakanu@users.noreply.github.com>
2023-10-01 10:51:47 +0530

    Time: 105 ms (100.00%), Space: 86.8 MB (100.00%) - LeetHub

--- 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.cpp
+++ 2874-maximum-value-of-an-ordered-triplet-ii/2874-maximum-value-of-an-ordered-triplet-ii.cpp
@@ -2,15 +2,19 @@ class Solution {
 public:
     long long maximumTripletValue(vector<int>& nums) {
         int n = nums.size();
+
+        // res is the maximum value of a triplet (A[i] - A[j]) * A[k].
+        // maxValue is the maximum value of pair A[i] - A[j]
+        // maxDiff is the maximum value of element A[i].
         
-        long long res = 0;
-        int maxValue = 0;
-        int maxDiff = 0;
+        long long res = 0; // multiplication of maxDiff and current value
+        int maxValue = 0; // maximum value found so far
+        int maxDiff = 0; // maximum difference found by maxValue and current element
         
         for(int &it : nums){
-            res = max(res, 1LL*maxDiff*it);
+            res = max(res, 1LL*maxDiff*it); // typecasting to long long
             maxDiff = max(maxDiff, maxValue - it);
-            maxValue = max(maxValue, it);
+            maxValue = max(maxValue, it); // think bottom to up, it will start making sense
         }
         return res;
     }
