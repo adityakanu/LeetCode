4f46334f16182c80276893ad3aa9bde242ee0597
Aditya Kanu <81138360+adityakanu@users.noreply.github.com>
2023-10-21 03:05:34 +0530

    Time: 3 ms (91.03%), Space: 7.2 MB (66.78%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length() != t.length())
+        return false;
+        int m1[256] = {0}, m2[256] = {0}, n = s.size();
+        for (int i = 0; i < n; ++i) {
+            if (m1[s[i]] != m2[t[i]]) return false;
+            m1[s[i]] = i + 1;
+            m2[t[i]] = i + 1;
+        }
+        return true;
+    }
+};
